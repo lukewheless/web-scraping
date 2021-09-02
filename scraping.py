de711e0cd3cc4bcf7798aa8b4d2f7ce049ea1fe1
lukewheless <lukewheless@gmail.com>
2021-09-02 09:10:04 -0500

    web

---
+++ scraping.py
@@ -0,0 +1,28 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+url = 'http://web.mta.info/developers/turnstile.html'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
